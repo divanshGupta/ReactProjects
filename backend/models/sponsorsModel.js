157e31474eb049557b0c0f9605a3f63efc72a48c
Harsh Srivastava <srivastavah240@gmail.com>
2023-12-18 17:20:57 +0530

    updated schema for sponsor

--- backend/models/sponsorsModel.js
+++ backend/models/sponsorsModel.js
@@ -1,4 +1,51 @@
+// import mongoose from "mongoose";
+
+// const sponsorSchema = mongoose.Schema(
+//     {
+//         orgName: {
+//             type: String,
+//             required: true,
+//         },
+//         orgType: {
+//             type: String,
+//             required: true,
+//         },
+//         industry: {
+//             type: String,
+//             required: true,
+//         },
+//         budget: {
+//             type: Number
+//         },
+//         socials:{
+//             type:String
+//         }
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
+
+// export const Sponsor = mongoose.model('Sponsor', sponsorSchema);
 import mongoose from "mongoose";
+
+const socialsSchema = mongoose.Schema(
+    {
+        platform: {
+            type: String,
+            required: true,
+            enum: ["LinkedIn", "Instagram", "Facebook"], // Specify the allowed platforms
+        },
+        options: {
+            followers: {
+                type: String,
+            },
+            following: {
+                type: String,
+            },
+        },
+    }
+);
 
 const sponsorSchema = mongoose.Schema(
     {
@@ -15,8 +62,9 @@ const sponsorSchema = mongoose.Schema(
             required: true,
         },
         budget: {
-            type: Number
-        }
+            type: Number,
+        },
+        socials: [socialsSchema], // Use an array to allow multiple socials entries
     },
     {
         timestamps: true,
--- backend/routes/sponsorRoute.js
+++ backend/routes/sponsorRoute.js
@@ -1,124 +1,244 @@
+// import express from 'express';
+// import { Sponsor } from '../models/sponsorsModel.js';
+
+// const router = express.Router();
+
+// //Route for getting all sponsors from database
+// router.get('/all', async (request, response) =>{
+//     try{
+//         const sponsors = await Sponsor.find({});
+//         // console.log(sponsors);
+//         return response.status(200).json({
+//             count: sponsors.length,
+//             data: sponsors,
+//         });
+//     }catch(error){
+//         // console.log(error.message);
+//         console.log(error.message);
+//         response.status(500).send( {message:error.message} );
+//     }
+//     // res.sendFile(path.join(__dirname, '../frontend/index.html'));
+// });
+
+// //Route for getting a specific sponsor using it's id
+// router.get('/:id', async (request, response) =>{
+//     try{
+//         const {id}  = request.params;
+//         if(!id){
+//             return response.status(400).json({ message: 'Invalid ID' });
+//         }
+//         // console.log("Received id: " + id); 
+
+//         const sponsor = await Sponsor.findById(id);
+//         return response.status(200).json(sponsor);
+//     }catch(error){
+//         // console.log("TENU"); //for debugging/checking error
+//         console.log(error.message);
+//         response.status(500).send( {message:error.message} );
+//     }
+// });
+
+// //Route to post a sponsor in the database
+// router.post('/', async(request, response) =>{
+//     try{
+//         if(!request.body.orgName || 
+//             !request.body.orgType || 
+//             !request.body.industry ||
+//             !request.body.budget ||
+//             !request.body.socials
+
+//             ){
+//                 console.log("HERE ERR")
+//                 return response.status(400).send({
+//                 message: 'Send all required fields: orgName, orgType, industry, budget',
+//             });
+//         }
+//         const newSponsor = {
+//             orgName: request.body.orgName,
+//             orgType: request.body.orgType,
+//             industry: request.body.industry,
+//             budget: request.body.budget,
+//             socials: request.body.socials
+//         };
+
+//         const sponsor = await Sponsor.create(newSponsor);
+//         // console.log(newSponsor);
+
+//         return response.status(201).send(sponsor);
+//     } 
+//     catch (error){
+//         console.log(error.message);
+//         response.status(500).send( {message: error.message} );
+//     }
+// })
+
+// //Route to update a sponsor
+// router.put('/:id', async(request, response)=>{
+//     try{
+//         if(!request.body.orgName || 
+//             !request.body.orgType || 
+//             !request.body.industry ||
+//             !request.body.budget ||
+//             !request.body.socials
+//         ){
+//             return response.status(400).send({
+//                 message: 'Send all required fields: title, author, publishYear, budget',
+//             });
+//         }
+
+//         const { id } = request.params;
+//         const result = await Sponsor.findByIdAndUpdate(id, request.body);
+
+//         if(!result){
+//             return response.status(404).json({message: 'Sponsor not found.'});
+//         }
+
+//         return response.status(200).send({
+//             message: 'Sponsor updated successfully.',
+//             id: `${id}`,
+//             orgName: `${request.body.orgName}`
+//         });
+//     }
+//     catch (error){
+//         console.log(error.message);
+//         response.status(500).send( {message: error.message} );
+//     }
+// })
+
+// //Route for deleting a sponsor by id
+// router.delete('/:id', async (request, response)=>{
+//     try{
+
+//         const { id } = request.params;
+//         const result = await Sponsor.findByIdAndDelete(id);
+
+//         if(!result){
+//             return response.status(404).json({message: 'Sponsor not found.'});
+//         }
+
+//         return response.status(200).send({ message: 'Sponsor deleted successfully.'});
+
+//     }catch (error){
+//         console.log(error.message);
+//         response.status(500).send( {message: error.message} );
+//     }
+// })
+
+// export default router;
 import express from 'express';
 import { Sponsor } from '../models/sponsorsModel.js';
 
 const router = express.Router();
 
-//Route for getting all sponsors from database
-router.get('/all', async (request, response) =>{
-    try{
+// Route for getting all sponsors from the database
+router.get('/all', async (request, response) => {
+    try {
         const sponsors = await Sponsor.find({});
-        // console.log(sponsors);
         return response.status(200).json({
             count: sponsors.length,
             data: sponsors,
         });
-    }catch(error){
-        // console.log(error.message);
+    } catch (error) {
         console.log(error.message);
-        response.status(500).send( {message:error.message} );
+        response.status(500).send({ message: error.message });
     }
-    // res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-//Route for getting a specific sponsor using it's id
-router.get('/:id', async (request, response) =>{
-    try{
-        const {id}  = request.params;
-        if(!id){
+// Route for getting a specific sponsor using its id
+router.get('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        if (!id) {
             return response.status(400).json({ message: 'Invalid ID' });
         }
-        // console.log("Received id: " + id); 
 
         const sponsor = await Sponsor.findById(id);
         return response.status(200).json(sponsor);
-    }catch(error){
-        // console.log("TENU"); //for debugging/checking error
+    } catch (error) {
         console.log(error.message);
-        response.status(500).send( {message:error.message} );
+        response.status(500).send({ message: error.message });
     }
 });
 
-//Route to post a sponsor in the database
-router.post('/', async(request, response) =>{
-    try{
-        if(!request.body.orgName || 
-            !request.body.orgType || 
+// Route to post a sponsor in the database
+router.post('/', async (request, response) => {
+    try {
+        if (
+            !request.body.orgName ||
+            !request.body.orgType ||
             !request.body.industry ||
-            !request.body.budget
-
-            ){
-                console.log("HERE ERR")
-                return response.status(400).send({
-                message: 'Send all required fields: orgName, orgType, industry, budget',
+            !request.body.budget ||
+            !request.body.socials
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields and array: orgName, orgType, industry, budget, socials',
             });
         }
+
         const newSponsor = {
             orgName: request.body.orgName,
             orgType: request.body.orgType,
             industry: request.body.industry,
             budget: request.body.budget,
+            socials: request.body.socials,
         };
 
         const sponsor = await Sponsor.create(newSponsor);
-        // console.log(newSponsor);
-
         return response.status(201).send(sponsor);
-    } 
-    catch (error){
+    } catch (error) {
         console.log(error.message);
-        response.status(500).send( {message: error.message} );
+        response.status(500).send({ message: error.message });
     }
-})
+});
 
-//Route to update a sponsor
-router.put('/:id', async(request, response)=>{
-    try{
-        if(!request.body.orgName || 
-            !request.body.orgType || 
+// Route to update a sponsor
+router.put('/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.orgName ||
+            !request.body.orgType ||
             !request.body.industry ||
-            !request.body.budget 
-        ){
+            !request.body.budget ||
+            !request.body.socials
+        ) {
             return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear, budget',
+                message: 'Send all required fields: orgName, orgType, industry, budget, socials',
             });
         }
 
         const { id } = request.params;
         const result = await Sponsor.findByIdAndUpdate(id, request.body);
 
-        if(!result){
-            return response.status(404).json({message: 'Sponsor not found.'});
+        if (!result) {
+            return response.status(404).json({ message: 'Sponsor not found.' });
         }
 
         return response.status(200).send({
             message: 'Sponsor updated successfully.',
             id: `${id}`,
-            orgName: `${request.body.orgName}`
+            orgName: `${request.body.orgName}`,
         });
-    }
-    catch (error){
+    } catch (error) {
         console.log(error.message);
-        response.status(500).send( {message: error.message} );
+        response.status(500).send({ message: error.message });
     }
-})
+});
 
-//Route for deleting a sponsor by id
-router.delete('/:id', async (request, response)=>{
-    try{
-
+// Route for deleting a sponsor by id
+router.delete('/:id', async (request, response) => {
+    try {
         const { id } = request.params;
         const result = await Sponsor.findByIdAndDelete(id);
 
-        if(!result){
-            return response.status(404).json({message: 'Sponsor not found.'});
+        if (!result) {
+            return response.status(404).json({ message: 'Sponsor not found.' });
         }
 
-        return response.status(200).send({ message: 'Sponsor deleted successfully.'});
-
-    }catch (error){
+        return response.status(200).send({ message: 'Sponsor deleted successfully.' });
+    } catch (error) {
         console.log(error.message);
-        response.status(500).send( {message: error.message} );
+        response.status(500).send({ message: error.message });
     }
-})
+});
 
 export default router;
